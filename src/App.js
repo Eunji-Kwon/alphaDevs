1fd8c653b9017124a05c7503e088f79703a02385
Eunji Kwon <sanduel97@gmail.com>
2023-11-15 03:00:53 -0500

    Nav Bar v02

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import {BrowserRouter as Router, Route, Link, Routes}
                     from 'react-router-dom';
 
 
-import EditList from './components/editlist.component';
-import AddTodo from './components/addtodo.component';
-import TodoLists from './components/todolists.component';
+// import EditList from './components/editlist.component';
+// import AddTodo from './components/addtodo.component';
+// import TodoLists from './components/todolists.component';
 import Navigation from './components/navigation.component';
+import Home from './components/home.component';
 
 function App() {
   return (
     <Router>
        <Navigation/>
     <Routes>
-      <Route path='/' Component={TodoLists}/>
-      <Route path='/edit/:id' Component={EditList}/>
-      <Route path='/add' Component={AddTodo}/>
+      {/* <Route path='/' Component={TodoLists}/> */}
+      {/* <Route path='/edit/:id' Component={EditList}/>
+      <Route path='/add' Component={AddTodo}/> */}
+
+      <Route path='/' Component={Home}/>
     </Routes>
 
 
