1d0a57c08e3d8d2a6ad8087de870ec121c8d79d0
Eunji (Silvia) Kwon <sandeul97@gmail.com>
2023-11-20 13:32:40 -0500

    LogIn Structure

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,10 +1,32 @@
 import React,{Component} from "react";
 
 export default class Login extends Component{
-    render(){
-        return(
-            <div>
-                <p>LogIn Page</p>
+   
+     render() {
+        return (
+            <div class="container">
+                <div class="col-lg-4">
+                    <p>LogIn Page</p>
+                </div>
+                <div class="col-lg-6">
+                    <form onSubmit={this.onSubmit}>
+                   
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Email</label>
+                            <input type="text" class="form-control" value="Email"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Password</label>
+                            <input type="text" class="form-control" value="password" />
+                        </div>
+
+                        
+                        <button type="submit" class="btn btn-primary">Login</button>
+                      
+                    </form>
+                </div>
+                <button type="submit">Sign In</button>
             </div>
         )
     }
