f02458eb38d6330a3591cea86bbdbeb271c91a68
Arash Karimi <79arashkarimi@gmail.com>
2023-11-29 20:23:08 -0500

    almost done 2

--- backend/routes/reservationRoutes.js
+++ backend/routes/reservationRoutes.js
@@ -4,9 +4,9 @@ const reservationController = require('../controllers/reservationController');
 const reservation = require('../models/reservation');
 
 //router.post('/', reservationController.createReservation);
-router.get('/:email', reservationController.getReservationByEmail);
+//router.get('/:email', reservationController.getReservationByEmail);
 
 router.post('/create_re', reservationController.createReservation);
-
+router.get('/:email', reservation.getReservationByEmail);
 
 module.exports = router;
