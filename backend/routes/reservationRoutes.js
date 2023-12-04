14bccd619c6f0cf9bdf675b17f823f0346481015
Arash Karimi <79arashkarimi@gmail.com>
2023-12-04 14:18:48 -0500

    it runs but it dont work

--- backend/routes/reservationRoutes.js
+++ backend/routes/reservationRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const reservationController = require('../controllers/reservationController');
-const reservation = require('../models/reservation');
 
 //router.post('/', reservationController.createReservation);
 //router.get('/:email', reservationController.getReservationByEmail);
 
 router.post('/create_re', reservationController.createReservation);
-router.get('/:email', reservation.getReservationByEmail);
+router.get('/:email', reservationController.getReservationByEmail);
 
 module.exports = router;
